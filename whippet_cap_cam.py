7aad42deafb2927aaeae34670addb99138df6370
Your user name <your.email@gmail.ho.hum>
2016-02-22 12:43:03 +0000

    adjust camera settings

--- whippet_cap_cam.py
+++ whippet_cap_cam.py
@@ -20,6 +20,15 @@ def get_image():
   global mlp, ip, outlayer, ready_flag
   with picamera.PiCamera() as camera:
     camera.resolution = (256, 256)
+    # try to fix the exposure to stop the image 'changing'
+    camera.frame_rate = 20
+    camera.iso = 400
+    time.sleep(1.0)
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
     with picamera.array.PiRGBArray(camera) as output:
       while True: # loop for ever
         if not ready_flag:
@@ -27,7 +36,7 @@ def get_image():
           camera.capture(output, 'rgb')
           im = output.array
           hog = ip.get_hog(im)
-          outlayer = mlp.feedForward(hog)
+          outlayer = mlp.feed_forward(hog)
           ready_flag = True
         time.sleep(0.1) 
 
@@ -53,7 +62,7 @@ hog = np.zeros((16, 128, 3), dtype=np.uint8)
 hidden = np.zeros((8, 8, 3), dtype=np.uint8)
 
 ########################################################################
-DISPLAY = pi3d.Display.create(x=50, y=50, background=(0.1, 0.1, 0.1, 1.0))
+DISPLAY = pi3d.Display.create(x=250, y=50, background=(0.1, 0.1, 0.1, 1.0))
 CAM = pi3d.Camera()
 shader = pi3d.Shader('uv_flat')
 lgtshd = pi3d.Shader('mat_light')
@@ -62,7 +71,7 @@ pi3d.opengles.glDisable(pi3d.GL_CULL_FACE)
 grey_tex = pi3d.Texture(grey, mipmap=False)
 sobel_tex = pi3d.Texture(sobel, mipmap=False)
 hog_tex = pi3d.Texture(hog, mipmap=False)
-hidden_tex = pi3d.Texture(hidden, mipmap=False)
+hidden_tex = pi3d.Texture(hidden, mipmap=True)
 
 grey_sprite = pi3d.ImageSprite(grey_tex, shader, w=2.5, h=2.5, x=-1.2, y=1.0, z=10.0)
 sobel_sprite = pi3d.ImageSprite(sobel_tex, shader, w=2.5, h=2.5, x=-0.0, y=0.5, z=8.0)
@@ -82,7 +91,8 @@ str_list = [pi3d.String(font=font, string='Border Terrier', x=-1.0, y=1.0, z=3.0
 for st in str_list:
   st.set_shader(shader)
 
-arrow = pi3d.Cone(radius=0.05, height=0.3, x=0.0, y=-2.0, z=3.0, rz=90.0)
+arrow_new = arrow_y = -2.0
+arrow = pi3d.Cone(radius=0.05, height=0.3, x=0.0, y=arrow_y, z=3.0, rz=90.0)
 arrow.set_shader(lgtshd)
 arrow.set_material((1.0, 0.9, 0.4))
 
@@ -103,6 +113,8 @@ while DISPLAY.loop_running():
   hidden_sprite.draw()
   for st in str_list:
     st.draw()
+  arrow_y = arrow_y * 0.995 + 0.005 * arrow_new
+  arrow.positionY(arrow_y)
   arrow.draw()
 
   if ready_flag:
@@ -111,9 +123,9 @@ while DISPLAY.loop_running():
     sobel_tex.update_ndarray(false_colour(ip.edges))
     hog_tex.update_ndarray(false_colour(ip.hog.reshape(16, 128)))
     hidden_tex.update_ndarray(false_colour(mlp.ah.reshape(8, 8)))
-
-    arrow.positionY(1.0 - 0.5 * outlayer.argmax())
-    
+    # target position for arrow
+    arrow_new = 1.0 - 0.5 * outlayer.argmax()
+    # colour and size of possible answers
     str_colours = false_colour(outlayer.reshape(5,1)) / 255.0
     for i, st in enumerate(str_list):
       st.set_material(str_colours[i, 0])
